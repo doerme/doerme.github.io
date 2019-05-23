598d3f0ad027b280df06a4ca209d2f3ba71d923e
梁晓明 <liangxiaoming@yy.com>
2019-05-23 17:16:35 +0800

    feat: 跑马更新

--- game/horse_run190307/main.js
+++ game/horse_run190307/main.js
@@ -45,6 +45,18 @@ window.boot = function () {
                 }
             }
         }
+
+        var subpackages = settings.subpackages;
+        for (var subId in subpackages) {
+            var uuidArray = subpackages[subId].uuids;
+            if (uuidArray) {
+                for (var k = 0, l = uuidArray.length; k < l; k++) {
+                    if (typeof uuidArray[k] === 'number') {
+                        uuidArray[k] = uuids[uuidArray[k]];
+                    }
+                }
+            }
+        }
     }
 
     function setLoadingDisplay () {
@@ -99,15 +111,6 @@ window.boot = function () {
             }
         }
 
-        // init assets
-        cc.AssetLibrary.init({
-            libraryPath: 'res/import',
-            rawAssetsBase: 'res/raw-',
-            rawAssets: settings.rawAssets,
-            packedAssets: settings.packedAssets,
-            md5AssetsMap: settings.md5AssetsMap
-        });
-
         var launchScene = settings.launchScene;
 
         // load scene
@@ -157,6 +160,16 @@ window.boot = function () {
         groupList: settings.groupList,
         collisionMatrix: settings.collisionMatrix,
     }
+
+    // init assets
+    cc.AssetLibrary.init({
+        libraryPath: 'res/import',
+        rawAssetsBase: 'res/raw-',
+        rawAssets: settings.rawAssets,
+        packedAssets: settings.packedAssets,
+        md5AssetsMap: settings.md5AssetsMap,
+        subpackages: settings.subpackages
+    });
 
     cc.game.run(option, onStart);
 };
